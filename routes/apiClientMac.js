3f39c7307bae8b89e21312ebaeb9734dd5129aac
Aaron Martin <sbbeachvball@gmail.com>
2015-12-25 13:47:44 -0800

    updates to api output

--- routes/apiClientMac.js
+++ routes/apiClientMac.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+// Really need to do a full check here like we do in apps, find a way to reuse that code
+var conf = require('../config.json');
+var data = require(conf.dhcpMap);
+
 /* GET users listing. */
 router.get('/:client', function(req, res, next) {
-  res.send('respond with a mac address for client:'+req.params.client);
+    var status = false;
+    var client = req.params.client;
+    if ( data.hasOwnProperty(client)){
+        res.send(data[client]);
+    }
+    else {
+        res.send('client: '+client+' not found');
+    }
 });
 
 module.exports = router;
